0d74e9378680fef8a8bd5d2cff042ffd5589f982
GMuskan <muskaang710@gmail.com>
2023-07-06 12:53:57 +0530

    Added media queries to home.css

--- pustaka/src/Pages/Home/Home.jsx
+++ pustaka/src/Pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import "./Home.css"
 import { useContext, useEffect } from "react";
 import { DataContext } from "../../Contexts/DataContext";
 import { changeTitle } from "../../utils/commonUtils";
+import homeImage from "../../Assets/home-img.jpg";
+import linkedInImage from "../../Assets/linkedin.png";
+import twitterImage from "../../Assets/twitter.png";
 
 const getActiveStyle = ({ isActive }) => ({
     margin: "1rem 0",
@@ -24,15 +27,15 @@ export const Home = () => {
         <>
             <div className="home">
                 <div className="header">
-                    <img src="https://pustaka-react.netlify.app/static/media/home-img.839d5b46.jpg" alt="book-cover" height="400px" />
+                    <img src={homeImage} alt="book-cover" />
                     <div className="image-overlay">
                         <p>Welcome to Pustaka,</p>
-                        <h1>For All Your Reading Needs</h1>
+                        <p className="sub-heading">For All Your Reading Needs</p>
                         <button className="shop-now-btn" onClick={() => navigate("/products")}>SHOP NOW</button>
                     </div>
                 </div>
                 <div className="categories">
-                    <h1>Featured Book Categories</h1>
+                    <p className="featured-categories">Featured Book Categories</p>
                     <p>There are many categories of books available at Pustaka. Choose your favorite one now.</p>
                     <div className="book-categories">
                         {categories && categories.map(({ _id, categoryName, description }) => (
@@ -47,15 +50,17 @@ export const Home = () => {
                     <div className="About">
                         <h2>Pustaka</h2>
                         <p>Fill your house with stacks of books, in all the crannies and all the nooks.</p>
-                        <p>Privacy Policy</p>
-                        <p>Terms of Use</p>
-                        <p>© 2022 Pustaka</p>
+                        {/* <p>Privacy Policy</p>
+                        <p>Terms of Use</p> */}
+                        <p>© 2023 Pustaka</p>
                     </div>
                     <div className="connect">
                         <p>Connect</p>
-                        <button><NavLink style={getActiveStyle} to="https://github.com/GMuskan">GitHub</NavLink></button>
-                        <button><NavLink style={getActiveStyle} to="https://twitter.com/Muskaan58571762">Twitter</NavLink></button>
-                        <button><NavLink style={getActiveStyle} to="https://www.linkedin.com/in/muskan-gupta-75482b166/">LinkedIn</NavLink></button>
+                        <div className="connect-buttons">
+                            <button><NavLink style={getActiveStyle} to="https://github.com/GMuskan"><i className="fa fa-github-square" style={{ fontSize: "22px" }} aria-hidden="true"></i></NavLink></button>
+                            <button><NavLink style={getActiveStyle} to="https://www.linkedin.com/in/muskan-gupta-75482b166/"><img src={linkedInImage} alt="linkedin-icon" /></NavLink></button>
+                            <button><NavLink style={getActiveStyle} to="https://twitter.com/Muskaan58571762"><img src={twitterImage} alt="twitter-icon" /></NavLink></button>
+                        </div>
                     </div>
                     <div className="resources">
                         <p>Resources</p>
@@ -63,8 +68,8 @@ export const Home = () => {
                             <button style={{ color: "white" }} onClick={() => navigate("/signup")}>
                                 Sign  Up
                             </button>
-                        </div>
-                        <div>
+                        {/* </div>
+                        <div> */}
                             <button style={{ color: "white" }} onClick={() => navigate("/login")}>
                                 Sign In
                             </button>
